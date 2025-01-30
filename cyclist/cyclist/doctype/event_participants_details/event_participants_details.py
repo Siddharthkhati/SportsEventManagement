fb055bc9ad914d246c34fbce2840ad8f24c8eadc
ManyaGirdhar <girdharmanya28@gmail.com>
2025-01-30 14:17:54 +0530

    event participants doctype created

---
+++ cyclist/cyclist/doctype/event_participants_details/event_participants_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Siddharth and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EventparticipantsDetails(Document):
+	pass
