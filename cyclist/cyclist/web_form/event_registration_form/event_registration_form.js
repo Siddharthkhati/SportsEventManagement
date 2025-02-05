4795e40579a33db7a71420063474da917dc22319
ManyaGirdhar <girdharmanya28@gmail.com>
2025-02-05 14:08:59 +0530

    notification and fields added in webform

--- cyclist/cyclist/web_form/event_registration_form/event_registration_form.js
+++ cyclist/cyclist/web_form/event_registration_form/event_registration_form.js
@@ -17,8 +17,11 @@ frappe.ready(function() {
                     frappe.web_form.set_value("event", event_name);
                     // code for setting championship name from the event doctype
                     frappe.web_form.set_value("championship",data.championship);
+                    frappe.web_form.set_value("champ",data.championship);
                     frappe.web_form.set_value("gender", data.category);
                     frappe.web_form.set_value("age_group", data.age_group);
+                    frappe.web_form.set_value("location", data.location);
+                    frappe.web_form.set_value("date", data.date);
 
                 }
             }
@@ -35,6 +38,21 @@ frappe.ready(function() {
                 }
             }
         });
+
+        // name_of_championship = frappe.web_form.get_value("championship");
+        // frappe.call({
+        //     method: "cyclist.api.get_championship_details",
+        //     args: { name_of_championship: name_of_championship },
+        //     callback: function(response) {
+        //         if (response.message) {
+        //             let data = response.message;
+                    
+        //             // Auto-fill License Web Form fields
+        //             frappe.web_form.set_value("location", data.location);
+        //             frappe.web_form.set_value("date", data.date);
+        //         }
+        //     }
+        // });
 
         frappe.call({
             method: "cyclist.api.get_full_name",
